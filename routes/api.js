6c94f29a0501dd3bb1c98cf382dd400a995d4b07
hhutku <hhutku.wa@gmail.com>
2021-02-12 18:07:52 -0800

    api routes update

--- routes/api.js
+++ routes/api.js
@@ -4,11 +4,29 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/api/workouts", (req, res) => {
-
+  Workout.create({})
+    .then((workoutDB) => {
+      res.json(workoutDB);
+    })
+    .catch(({ message }) => {
+      console.log(message);
+    });
 });
 
 router.put("/api/workouts/:id", (req, res) => {
- 
+  Workout.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      $push: { exercises: req.body }
+    },
+    { new: true }
+  )
+    .then((workoutDB) => {
+      res.json(workoutDB);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 router.get("/api/workouts/range", (req, res) => {
