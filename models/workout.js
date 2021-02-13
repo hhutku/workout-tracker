1d019f11b0edf0076709741da185eeadd8c51626
hhutku <hhutku.wa@gmail.com>
2021-02-12 18:10:11 -0800

    update workout.js

--- models/workout.js
+++ models/workout.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema(
   {
-    day: { type: Date },
+    day: { type: Date , default: Date.now },
     exercises: [
       {
         type: {
