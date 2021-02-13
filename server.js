f7a47d2e47367dd93e5e7f718d7b62ace00264e1
hhutku <hhutku.wa@gmail.com>
2021-02-12 17:28:37 -0800

    server.js updated

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+const route1 = require("./routes/api");
+const route2 = require("./routes/html");
 
-const router = require("./routes/html.js");
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,8 +13,18 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(router);
+app.use(route1);
+app.use(route2);
 
+
+ mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  
+  }
+);
 
 
 app.listen(PORT, () => {
