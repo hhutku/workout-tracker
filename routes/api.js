01bc7cd792d2d71ab48607540f6933f0101399b3
hhutku <hhutku.wa@gmail.com>
2021-02-12 16:51:30 -0800

    api routes

---
+++ routes/api.js
@@ -0,0 +1,22 @@
+const Workout = require("../models/workout");
+
+const express = require("express");
+const router = express.Router();
+
+router.post("/api/workouts", (req, res) => {
+
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+ 
+});
+
+router.get("/api/workouts/range", (req, res) => {
+ 
+});
+
+router.get("/api/workouts", (req, res) => {
+
+});
+
+module.exports = router;
